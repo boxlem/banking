9a67a18ff15125e3319024e333e81374ddfebe98
boxlem <85836462+boxlem@users.noreply.github.com>
2024-09-08 01:42:54 +0800

    Add files via upload

---
+++ app/(root)/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
+
+const Dashboard = async () => {
+  const loggedIn = await getLoggedInUser();
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome back"
+            user={loggedIn?.name || "Guest"}
+            subtext="Access and manage your account and transactions efficiently"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1019.06}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 126.06 }, { currentBalance: 500.9 }]}
+      />
+    </section>
+  );
+};
+
+export default Dashboard;
